bbdeaa9a944d6e16a8437619634b5685a099f2c8
LXY2876 <63182973+LXY2876@users.noreply.github.com>
2024-09-11 23:51:13 +0800

    Add files via upload

--- linear_cof.m
+++ linear_cof.m
@@ -1,4 +1,4 @@
-function [u_a,w_a,u_b,w_b] = linear_cof(X,Y,x1,y1,x2,y2,angle,theta)
+function [normal_cof_a,normal_cof_b,tangl_cof_a,tangl_cof_b] = linear_cof(X,Y,x1,y1,x2,y2,angle,theta,flag)
 % 计算影响系数 
 % 将控制面细分为n段求数值积分
 %控制点j坐标
@@ -14,27 +14,29 @@ r2=sqrt((X_r-s/2)^2+Y_r^2);
 % angle2=atan(Y_r/(X_r-s/2));
 angle1=atan2(Y_r,(X_r+s/2));
 angle2=atan2(Y_r,(X_r-s/2));
-theta_p=theta-angle;%控制点处的切向方向在面源坐标系下的角度
+delta_theta=theta-angle;%控制点处的切向方向在面源坐标系下的角度
 % cof=(X_r*log(r2/r1)+s*(log(r1*r2)-2)/2+Y_r*(angle2-angle1))/(2*pi);
 %计算面源在控制点处的速度，面源坐标系下
 %均匀面源
+if X_r==0 && Y_r==0
+    angle2=pi;
+end
 up_a=1/(2*pi)*(angle2-angle1)/2+1/(2*pi)*(Y_r*log(r2/r1)+X_r*(angle2-angle1))/(-s);
 wp_a=1/(2*pi)*log(r2/r1)/2+1/(2*pi)*(X_r*log(r2/r1)+s-Y_r*(angle2-angle1))/(-s);
 up_b=1/(2*pi)*(angle2-angle1)/2+1/(2*pi)*(Y_r*log(r2/r1)+X_r*(angle2-angle1))/(s);
 wp_b=1/(2*pi)*log(r2/r1)/2+1/(2*pi)*(X_r*log(r2/r1)+s-Y_r*(angle2-angle1))/(s);
-if(Y_r==0 && X_r==0)
-    up_a=0.25;
-    wp_a=-1/(2*pi);
-    up_b=0.25;
-    wp_b=1/(2*pi)
-end
 % cof=dot([up,wp],[cos(theta_p),sin(th
 % eta_p)]);
-u_a=up_a*cos(angle)-wp_a*sin(angle);
-w_a=up_a*sin(angle)+wp_a*cos(angle);
-u_b=up_b*cos(angle)-wp_b*sin(angle);
-w_b=up_b*sin(angle)+wp_b*cos(angle);
-
+if flag==1
+%     u_a=up_a*cos(theta_p)-wp_a*sin(theta_p);
+%     w_a=up_a*sin(theta_p)+wp_a*cos(theta_p);
+    normal_cof_a=dot([up_a,wp_a],[cos(delta_theta+pi/2),sin(delta_theta+pi/2)]);
+%     u_b=up_b*cos(theta_p)-wp_b*sin(theta_p);
+%     w_b=up_b*sin(theta_p)+wp_b*cos(theta_p);
+    normal_cof_b=dot([up_b,wp_b],[cos(delta_theta+pi/2),sin(delta_theta+pi/2)]);
+    tangl_cof_a=dot([up_a,wp_a],[cos(delta_theta),sin(delta_theta)]);
+    tangl_cof_b=dot([up_b,wp_b],[cos(delta_theta),sin(delta_theta)]);
+end
 %将面源坐标系下的速度投影到控制点的法向
 
 
--- solver.m
+++ solver.m
@@ -33,14 +33,12 @@ RHS=zeros(2*n-1,1);
 for i =1:2*n-2
     RHS(i)=-dot(V,[cos(theta(i)+pi/2),sin(theta(i)+pi/2)]);
     for j=1:2*n-2
-        if i==166 && j==166
-            b=0;
-        end
-        [u_a,w_a,u_b,w_b]=linear_cof(ctrl_x(i),ctrl_y(i),surf_x(j),surf_y(j),surf_x(j+1),surf_y(j+1),theta(j),theta(i)); 
-        A(i,j)=A(i,j)+dot([u_a,w_a],[cos(theta(i)+pi/2),sin(theta(i)+pi/2)]);
-        A(i,j+1)=A(i,j+1)+dot([u_b,w_b],[cos(theta(i)+pi/2),sin(theta(i)+pi/2)]);            
-        B(i,j)=B(i,j)+dot([u_a,w_a],[cos(theta(i)),sin(theta(i))]);
-        B(i,j+1)=B(i,j+1)+dot([u_b,w_b],[cos(theta(i)),sin(theta(i))]);
+   
+        [n_a,n_b,t_a,t_b]=linear_cof(ctrl_x(i),ctrl_y(i),surf_x(j),surf_y(j),surf_x(j+1),surf_y(j+1),theta(j),theta(i),1); 
+        A(i,j)=A(i,j)+n_a;
+        A(i,j+1)=A(i,j+1)+n_b;            
+        B(i,j)=B(i,j)+t_a;
+        B(i,j+1)=B(i,j+1)+t_b;
     end
 end
 A(2*n-1,n-1)=1;A(2*n-1,n+1)=1; A(2*n-1,n)=2; 
